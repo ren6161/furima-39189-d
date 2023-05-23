c5ef36384b76075d0a09b8ca9c664f86d8d6f239
ren6161 <130984926+ren6161@users.noreply.github.com>
2023-05-24 00:50:56 +0900

    エラーメッセージ重複修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,7 +21,6 @@ class Item < ApplicationRecord
   validates :days_to_ship_id,    presence: true
   validates :price,              presence: true
   validates :price,              numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :price,              numericality: { with: /\A[0-9]+\z/}
   validates :category_id,        numericality: { other_than: 1 }
   validates :condition_id,       numericality: { other_than: 1 }
   validates :shipping_area_id,   numericality: { other_than: 1 }
