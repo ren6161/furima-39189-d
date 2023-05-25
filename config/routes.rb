d0aba54cf21199f4f2642d292a6fbcdd9d97336e
ren6161 <130984926+ren6161@users.noreply.github.com>
2023-05-25 15:52:19 +0900

    商品情報編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  get 'items/new'
   devise_for :users
    root to: 'items#index'
-   resources :items do
-   end
+   resources :items, only: [:new, :create, :show, :edit, :update, :destroy ]
 end
