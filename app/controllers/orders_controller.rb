e6091b120b6b70633ea7673bb3d558bf3baa5e0c
ren6161 <130984926+ren6161@users.noreply.github.com>
2023-05-25 20:03:09 +0900

    コントローラーとビュー

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,34 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :non_purchased_item, only: [:index, :create]
+
+  def index
+    @order_form = OrderForm.new
+  end
+  
+  def create
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_form).permit(:postal_code, :shipping_area_id, :city, :block_number, :apartment_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,        # 商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy'             # 通貨の種類（日本円）
+    )
+  end
+end
