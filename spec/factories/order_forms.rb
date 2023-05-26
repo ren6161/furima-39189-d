6ad83cc7914b4b41d9b890f332ea3d169e7b878d
ren6161 <130984926+ren6161@users.noreply.github.com>
2023-05-26 14:50:37 +0900

    決済機能と修正など

--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -8,6 +8,6 @@ FactoryBot.define do
     block_number { Faker::Address.street_address }
     apartment_name { Faker::Address.street_address }
     phone_number { Faker::Number.decimal_part(digits: 11) }
-    #token { Faker::Internet.password(min_length: 20, max_length: 30) }
+    token { Faker::Internet.password(min_length: 20, max_length: 30) }
   end
 end
